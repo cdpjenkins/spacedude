7f75f0b3be67cd971d0414485057c79f6300f7c0
cdpjenkins <cdpjenkins@gmail.com>
2022-04-10 19:45:13 +0100

    Pull main look into App class

---
+++ src/App.hpp
@@ -0,0 +1,24 @@
+#ifndef APP_HPP
+#define APP_HPP
+
+#include "Dude.hpp"
+#include "SDLContext.hpp"
+
+class App {
+public:
+    App(SDLContext *sdl);
+
+    Dude dude = Dude(500.0f, 500.0f, 0.0f);
+
+    void main_loop();
+
+private:
+    const int JOYSTICK_DEAD_ZONE = 1000;
+
+    SDLContext *sdl;
+
+    int joystick_x = 0;
+    int joystick_y = 0;
+};
+
+#endif // APP_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -5,96 +5,17 @@
 
 #include "Dude.hpp"
 #include "SDLContext.hpp"
+#include "App.hpp"
 
 using namespace std;
-
-const int JOYSTICK_DEAD_ZONE = 1000;
-int joystick_x = 0;
-int joystick_y = 0;
 
 SDL_Texture *dude_texture;
 
 int main(int argc, char** argv) {
-    Dude dude(500.0f, 500.0f, 0.0f);
-
     SDLContext sdl;
+    App app = App(&sdl);
 
-    bool keys[SDL_NUM_SCANCODES];
-    memset(keys, 0, sizeof(keys));
-
-    SDL_Event e;
-    bool quit = false;
-    Uint32 last_ticks = 0;
-    while (!quit){
-        Uint32 new_ticks = SDL_GetTicks();
-        cout << new_ticks - last_ticks << endl;
-        last_ticks = new_ticks;
-
-        while (SDL_PollEvent(&e)){
-            switch (e.type) {
-                case SDL_QUIT:
-                    quit = true;
-                    break;
-                case SDL_KEYUP:
-                    keys[e.key.keysym.scancode] = false;
-                    break;
-                case SDL_KEYDOWN:
-                    keys[e.key.keysym.scancode] = true;
-                    break;
-                case SDL_JOYAXISMOTION:
-                    if(e.jaxis.axis == 0 ) {
-                        cout << "Joystick x: " << e.jaxis.value << endl;
-                        if (abs(e.jaxis.value) > JOYSTICK_DEAD_ZONE) {
-                            joystick_x = e.jaxis.value;
-                        } else {
-                            joystick_x = 0;
-                        }
-                    } else if (e.jaxis.axis == 1) {
-                        cout << "Joystick y: " << e.jaxis.value << endl;
-                        if (abs(e.jaxis.value) > JOYSTICK_DEAD_ZONE) {
-                            joystick_y = e.jaxis.value;
-                        } else {
-                            joystick_y = 0;
-                        }
-                    } else {
-                        cout << "Something else: " << e.jaxis.axis << endl;
-                    }
-
-            }
-        }
-
-        if (joystick_x != 0 && joystick_y != 0) {
-            dude.theta = atan2(joystick_x, -joystick_y) * 180 / M_PI;
-        }
-
-        if (keys[SDL_SCANCODE_UP]) {
-            dude.accelerate_forwards();
-        }
-        if (keys[SDL_SCANCODE_DOWN]) {
-            // dude.y += 1;
-        }
-        if (keys[SDL_SCANCODE_RIGHT]) {
-            dude.theta += 1;
-        }
-        if (keys[SDL_SCANCODE_LEFT]) {
-            dude.theta -= 1;
-        }
-        if (keys[SDL_SCANCODE_Q]) {
-            quit = true;
-        }
-
-        dude.move();
-
-        // Uint32 time_before_step = SDL_GetTicks();
-        // grid.step();
-        Uint32 time_after_step_before_draw = SDL_GetTicks();
-        // cout << "Time to step: " << (time_after_step_before_draw - time_before_step) << endl;
-        sdl.render(&dude);
-        Uint32 time_after_draw = SDL_GetTicks();
-        // cout << "Time to draw: " << (time_after_draw - time_after_step_before_draw) << endl;
-    }
-
-    //Destroy window    
+    app.main_loop();
 
     return 0;
 }
